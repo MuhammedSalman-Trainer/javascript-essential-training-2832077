44d2a80de88c9985befab0a29eec479448b68046
Muhammed Salman <muhammedsalman.techtutor@gmail.com>
2024-04-18 19:07:49 +0530

    practice update

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,15 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const salman={
+    species:"human",
+    senses:6,
+    isFunny:false,
+    isRomantic:true,
+    efficiency:{
+        knowledge:0.75,
+        eating:1.00,
+        socialCircle:-0.80
+    }
+}
